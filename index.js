0a1a3965cef80c6dab7eea35025a3dd7da81b88d
Harman <scaleflowai25@gmail.com>
2026-05-06 20:49:56 +0530

    updated webhook

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const runFollowUps = require("./workers/followup");
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("🔥 REQUEST:", req.method, req.url);
+  next();
+});
 
 // OpenAI setup
 const openai = new OpenAI({
